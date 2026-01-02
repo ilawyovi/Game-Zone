0edda287468d787b70c1315bc207c8c7bd0d1797
@ilawyovi <taghavinima807@gmail.com>
2026-01-02 20:20:27 +0330

    Grid & NavBar

--- Game-Zone/src/App.tsx
+++ Game-Zone/src/App.tsx
@@ -1,8 +1,32 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import NavBar from "./NavBar";
 
 function App() {
-    return (
-  <Button colorScheme='blue'>Button</Button>
-  )
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" >
+       <NavBar />
+      </GridItem>
+
+      <Show above="lg">
+      <GridItem area="aside" bg="lightblue">
+        Aside
+      </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="lightgreen">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
-export default App
+
+export default App;
