b3fdbd6eb918f37f5b288a8cc18ef171a8531590
@ilawyovi <taghavinima807@gmail.com>
2026-01-03 13:02:42 +0330

    Game Grid

--- Game-Zone/src/components/GameGrid.tsx
+++ Game-Zone/src/components/GameGrid.tsx
@@ -1,43 +1,19 @@
-import React, { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  counts: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient.get<FetchGamesResponse>('/games')
-      .then(res => setGames(res.data.results))
-      .catch(err => setError(err.message));
-
-  }, []);
+ const {games, error} = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <ul>
-        {games.map(game => ( <li key={game.id}>{game.name}</li> ))}
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
       </ul>
     </>
   );
-
 };
 
-
 export default GameGrid;
-
-
-
-
-
