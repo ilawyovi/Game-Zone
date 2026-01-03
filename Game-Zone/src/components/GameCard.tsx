f18fa6fd5c16ff360b725dbf603bbc1bc7a0eb9b
@ilawyovi <taghavinima807@gmail.com>
2026-01-03 17:42:51 +0330

    Skeleton

--- Game-Zone/src/components/GameCard.tsx
+++ Game-Zone/src/components/GameCard.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import type { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -12,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
--- Game-Zone/src/components/GameGrid.tsx
+++ Game-Zone/src/components/GameGrid.tsx
@@ -1,19 +1,60 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
- const {games, error} = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ];
+
+  //   return (
+  //     <>
+  //       {isLoading &&
+  //         skeletons.map((skeleton) => (
+  //           <GameCardContainer>
+  //             <GameCardSkeleton key={skeleton} />
+  //           </GameCardContainer>
+  //         ))}
+  //       {error && <Text>{error}</Text>}
+  //       <SimpleGrid
+  //         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+  //         padding="10px"
+  //         spacing={10}
+  //       >
+  //         {games.map((game) => (
+  //           <GameCardContainer>
+  //             <GameCard key={game.id} game={game} />
+  //           </GameCardContainer>
+  //         ))}
+  //       </SimpleGrid>
+  //     </>
+  //   );
+  // };
+
+  if (error) return <Text>{error}</Text>;
 
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 , xl: 5 }} padding='10px' spacing={10}>
-        {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+      padding="10px"
+      spacing={10}
+    >
+      {isLoading &&
+        skeletons.map((_, index) => (
+          <GameCardContainer key={index}>
+            <GameCardSkeleton />
+          </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+
+      {!isLoading &&
+        games.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        ))}
+    </SimpleGrid>
   );
 };
 
