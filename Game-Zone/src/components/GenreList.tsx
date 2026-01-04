ca395b82236eaa5d1ed190a0a76080de6039296d
@ilawyovi <taghavinima807@gmail.com>
2026-01-04 16:18:57 +0330

    Geners Skeleton

--- Game-Zone/src/components/GenreList.tsx
+++ Game-Zone/src/components/GenreList.tsx
@@ -1,24 +1,39 @@
 import useGenres from "../hooks/useGenres";
-import {Text, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import { Text, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import { SkeletonText, SkeletonCircle } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, isLoading } = useGenres();
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
+  if (error) return null;
 
   return (
     <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Text fontSize='lg'>{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
+      {isLoading
+        ? skeletons.map((skeleton) => (
+            <ListItem key={skeleton} paddingY="5px">
+              <HStack>
+                <SkeletonCircle boxSize="32px" />
+                <SkeletonText noOfLines={1} width="100px" />
+              </HStack>
+            </ListItem>
+          ))
+        : data.map((genre) => (
+            <ListItem key={genre.id} paddingY="5px">
+              <HStack>
+                <Image
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                />
+                <Text fontSize="lg">{genre.name}</Text>
+              </HStack>
+            </ListItem>
+          ))}
     </List>
   );
 };
