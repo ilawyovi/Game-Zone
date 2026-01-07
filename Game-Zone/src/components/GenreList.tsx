2ca845bf6c2fc4141aa3e0ce1d25f3d17cce85fd
@ilawyovi <taghavinima807@gmail.com>
2026-01-07 18:40:33 +0330

    Highlight The Genre

--- Game-Zone/src/components/GenreList.tsx
+++ Game-Zone/src/components/GenreList.tsx
@@ -1,14 +1,15 @@
 import useGenres from "../hooks/useGenres";
-import { Text, HStack, Image, List, ListItem, Button } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Button } from "@chakra-ui/react";
 import { SkeletonText, SkeletonCircle } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 import type { Genre } from "../hooks/useGenres";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, error, isLoading } = useGenres();
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
@@ -35,7 +36,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
                   borderRadius={8}
                   src={getCroppedImageUrl(genre.image_background)}
                 />
-                <Button onClick={() => onSelectGenre(genre)} fontSize="lg" variant='link'>{genre.name}</Button>
+                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize="lg" variant='link'>{genre.name}</Button>
               </HStack>
             </ListItem>
           ))}
