406ed1a2eace02f4e9bb07d96ad5bd8a00ef899f
Your Name <taghavinima807@gmail.com>
2026-09-06 22:58:39 +0330

    Fix Placeholder Image Path

--- Game-Zone/src/services/image-url.ts
+++ Game-Zone/src/services/image-url.ts
@@ -1,4 +1,4 @@
-import noImage from '../assets/no-image-placeholder.webp';
+import noImage from "../assets/No-Image-Placeholder.webp";
 
 const getCroppedImageUrl = (url: string) => {
   if (!url) return noImage;
